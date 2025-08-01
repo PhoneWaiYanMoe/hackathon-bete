a4b6df133abe3bed4ea5b73640b96e7e8cb6c1e9
PhoneWaiYanMoe <wyan40653@gmail.com>
2025-08-01 10:09:25 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,525 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(WaterPuppetApp());
+}
+
+class WaterPuppetApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Water Puppet',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
+      ),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  int energy = 85;
+  int coins = 1250;
+  bool isPremium = false;
+  late AnimationController _bounceController;
+  late AnimationController _heartController;
+  late Animation<double> _bounceAnimation;
+  late Animation<double> _heartAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _bounceController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+    
+    _heartController = AnimationController(
+      duration: Duration(milliseconds: 1500),
+      vsync: this,
+    );
+    
+    _bounceAnimation = Tween<double>(
+      begin: 0.0,
+      end: 10.0,
+    ).animate(CurvedAnimation(
+      parent: _bounceController,
+      curve: Curves.easeInOut,
+    ));
+    
+    _heartAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _heartController,
+      curve: Curves.elasticOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _bounceController.dispose();
+    _heartController.dispose();
+    super.dispose();
+  }
+
+  void _onCharacterTap() {
+    HapticFeedback.lightImpact();
+    _heartController.forward().then((_) {
+      _heartController.reverse();
+    });
+    
+    setState(() {
+      if (energy > 5) {
+        energy -= 5;
+      }
+    });
+  }
+
+  void _onButtonTap(String action) {
+    HapticFeedback.selectionClick();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$action feature coming soon!'),
+        duration: Duration(seconds: 1),
+        backgroundColor: Colors.green.shade600,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF7FB069),
+              Color(0xFF4A7C59),
+              Color(0xFF2F5233),
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              // Top HUD
+              _buildTopHUD(),
+              
+              // Character Area
+              Expanded(
+                flex: 3,
+                child: _buildCharacterArea(),
+              ),
+              
+              // Main Action Button
+              _buildPlayGamesButton(),
+              
+              // Daily Reward
+              _buildDailyReward(),
+              
+              // Action Buttons Grid
+              _buildActionButtons(),
+              
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTopHUD() {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Energy Bar
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            decoration: BoxDecoration(
+              color: Colors.blue.shade400,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Icon(Icons.water_drop, color: Colors.white, size: 16),
+                SizedBox(width: 4),
+                Text(
+                  '$energy%',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          
+          // Coins
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+            decoration: BoxDecoration(
+              color: Colors.amber.shade400,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Icon(Icons.monetization_on, color: Colors.white, size: 16),
+                SizedBox(width: 4),
+                Text(
+                  '${coins.toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          
+          // Premium & Settings
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () => _onButtonTap('Premium'),
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.orange.shade400,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 4,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Icon(Icons.star, color: Colors.white, size: 20),
+                ),
+              ),
+              SizedBox(width: 8),
+              GestureDetector(
+                onTap: () => _onButtonTap('Settings'),
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade600,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 4,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Icon(Icons.settings, color: Colors.white, size: 20),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCharacterArea() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Stack(
+          children: [
+            // Character Background
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xFF87CEEB),
+                    Color(0xFF4682B4),
+                  ],
+                ),
+              ),
+            ),
+            
+            // Character
+            Center(
+              child: GestureDetector(
+                onTap: _onCharacterTap,
+                child: AnimatedBuilder(
+                  animation: _bounceAnimation,
+                  builder: (context, child) {
+                    return Transform.translate(
+                      offset: Offset(0, _bounceAnimation.value),
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.brown.shade300,
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 8,
+                              offset: Offset(0, 4),
+                            ),
+                          ],
+                        ),
+                        child: Stack(
+                          children: [
+                            // Character Face
+                            Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  // Hat
+                                  Container(
+                                    width: 80,
+                                    height: 30,
+                                    decoration: BoxDecoration(
+                                      color: Colors.brown.shade200,
+                                      borderRadius: BorderRadius.circular(40),
+                                    ),
+                                  ),
+                                  SizedBox(height: 10),
+                                  // Eyes
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Container(
+                                        width: 20,
+                                        height: 20,
+                                        decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          shape: BoxShape.circle,
+                                        ),
+                                      ),
+                                      SizedBox(width: 20),
+                                      Container(
+                                        width: 20,
+                                        height: 20,
+                                        decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          shape: BoxShape.circle,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(height: 15),
+                                  // Mouth
+                                  Container(
+                                    width: 30,
+                                    height: 15,
+                                    decoration: BoxDecoration(
+                                      color: Colors.red.shade400,
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            
+            // Heart Animation
+            Positioned(
+              top: 20,
+              right: 20,
+              child: AnimatedBuilder(
+                animation: _heartAnimation,
+                builder: (context, child) {
+                  return Transform.scale(
+                    scale: _heartAnimation.value,
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                      size: 30,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPlayGamesButton() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: ElevatedButton(
+        onPressed: () => _onButtonTap('Play Games'),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.red.shade500,
+          foregroundColor: Colors.white,
+          padding: EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+          elevation: 8,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.games, size: 24),
+            SizedBox(width: 10),
+            Text(
+              'PLAY GAMES',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDailyReward() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      child: ElevatedButton(
+        onPressed: () => _onButtonTap('Daily Reward'),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.amber.shade400,
+          foregroundColor: Colors.white,
+          padding: EdgeInsets.symmetric(vertical: 12),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          elevation: 4,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.card_giftcard, size: 20),
+            SizedBox(width: 8),
+            Text(
+              'Daily Reward Available!',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildActionButtons() {
+    final buttons = [
+      {'icon': Icons.restaurant, 'label': 'Feed', 'color': Colors.orange.shade400},
+      {'icon': Icons.pets, 'label': 'Pet', 'color': Colors.pink.shade300},
+      {'icon': Icons.music_note, 'label': 'Dance', 'color': Colors.green.shade400},
+      {'icon': Icons.camera_alt, 'label': 'Photo', 'color': Colors.blue.shade400},
+      {'icon': Icons.shopping_bag, 'label': 'Shop', 'color': Colors.amber.shade400},
+      {'icon': Icons.emoji_events, 'label': 'Awards', 'color': Colors.red.shade400},
+    ];
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: GridView.builder(
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 2.2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: buttons.length,
+        itemBuilder: (context, index) {
+          final button = buttons[index];
+          return GestureDetector(
+            onTap: () => _onButtonTap(button['label'] as String),
+            child: Container(
+              decoration: BoxDecoration(
+                color: button['color'] as Color,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    button['icon'] as IconData,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    button['label'] as String,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
